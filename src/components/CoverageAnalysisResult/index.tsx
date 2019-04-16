ccba17d51242dccd0ab3902e199682d7a061e7b2
Jni <jni.viens@gmail.com>
2019-04-16 00:08:50 -0400

    Adds a few BEM samples
    Nothing is really meaningful, but it should give a good example of a few
    techniques.

--- src/components/CoverageAnalysisResult/index.tsx
+++ src/components/CoverageAnalysisResult/index.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import Abf from "../../lib/abf/Abf";
 import InsuranceCoverage from "../../lib/coverage/InsuranceCoverage";
 
+import classes from "./styles.scss";
+
 interface CoverageAnalysisResultProps {
     abf: Abf;
     coverage: InsuranceCoverage;
@@ -9,16 +11,16 @@ interface CoverageAnalysisResultProps {
 
 const CoverageAnalysisResult = ({abf, coverage}: CoverageAnalysisResultProps) => {
     return (
-        <div>
-            <div>
-                <h2>Your current financial state</h2>
+        <div className={classes.results}>
+            <div className={classes.results__section}>
+                <h2 className={classes["results__section-title"]}>Your current financial state</h2>
                 <p>Salary: {abf.salary}</p>
                 <p>Number of kids: {abf.kids}</p>
                 <p>ZipCode: {abf.zipCode}</p>
             </div>
 
-            <div>
-                <h2>We suggest the following coverage</h2>
+            <div className={`${classes.results__section} ${classes["results__section--highlighted"]}`}>
+                <h2 className={classes["results__section-title"]}>We suggest the following coverage</h2>
                 <p>Temporary coverage: {coverage.temporaryCoverage} over {coverage.temporaryCoverageTimeInYears} years</p>
                 <p>Permanent coverage: {coverage.permanentCoverage}</p>
             </div>
--- src/components/CoverageForm/index.tsx
+++ src/components/CoverageForm/index.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Abf from "../../lib/abf/Abf";
 
+import classes from "./styles.scss";
+import formClasses from "../Form/styles.scss";
+
 interface CoverageFormProps {
     submitted(abf: Abf): void;
 }
@@ -50,18 +53,27 @@ class CoverageForm extends React.Component<CoverageFormProps, CoverageFormState>
         const {salary, kids, zipCode} = this.state;
 
         return (
-            <form onSubmit={e => this.submitAbf(e)}>
-                <label htmlFor="salary">Salary</label>
-                <input id="salary" type="numeric" value={salary} onChange={e => this.handleSalaryChange(e)}/>
+            <>
+                <h2 className={classes.title}>Explain your situation</h2>
+                <form onSubmit={e => this.submitAbf(e)}>
+                    <div className={formClasses.form__field}>
+                        <label className={formClasses.form__label} htmlFor="salary">Salary</label>
+                        <input className={formClasses.form__input} id="salary" type="numeric" value={salary} onChange={e => this.handleSalaryChange(e)}/>
+                    </div>
 
-                <label htmlFor="kids">How many kids</label>
-                <input id="kids" type="numeric" value={kids} onChange={e => this.handleKidsChange(e)}/>
+                    <div className={formClasses.form__field}>
+                        <label className={formClasses.form__label} htmlFor="kids">How many kids</label>
+                        <input className={formClasses.form__input} id="kids" type="numeric" value={kids} onChange={e => this.handleKidsChange(e)}/>
+                    </div>
 
-                <label htmlFor="zipCode">Zip code</label>
-                <input id="zipCode" value={zipCode} onChange={e => this.handleZipCodeChange(e)}/>
+                    <div className={formClasses.form__field}>
+                        <label className={formClasses.form__label} htmlFor="zipCode">Zip code</label>
+                        <input className={formClasses.form__input} id="zipCode" value={zipCode} onChange={e => this.handleZipCodeChange(e)}/>
+                    </div>
 
-                <input type="submit" value="Calculate my needs"/>
-            </form>
+                    <input type="submit" value="Calculate my needs"/>
+                </form>
+            </>
         );
     }
 }
